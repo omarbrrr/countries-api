104a6ed10ae879601cdf61ac3b44d5d303da0166
Omar Benavides <omar.benavides.rmz@gmail.com>
2021-01-28 05:55:36 -0600

    Fix layout paddings and text weights

--- src/components/Country.js
+++ src/components/Country.js
@@ -1,24 +1,27 @@
 import React from "react";
 
-export default function Country({ country }) {
+export default function Country({ country, setSelectedCountry }) {
   const { flag, name, population, region, capital } = country;
 
   return (
-    <div className="country">
+    <div className="country" onClick={() => setSelectedCountry(name)}>
       <div className="country__flag-container">
         <img src={flag} className="country__flag" alt="" />
       </div>
       <div className="country__content">
         <h2 className="country__title">{name}</h2>
+
         <p className="country__subtitle">
-          Population:{" "}
-          <span className="country__value">{population.toLocaleString()}</span>
+          <span className="country__value">Population: </span>
+          {population.toLocaleString()}
         </p>
         <p className="country__subtitle">
-          Region: <span className="country__value">{region}</span>
+          <span className="country__value">Region: </span>
+          {region}
         </p>
         <p className="country__subtitle">
-          Capital: <span className="country__value">{capital}</span>
+          <span className="country__value">Capital: </span>
+          {capital}
         </p>
       </div>
     </div>
