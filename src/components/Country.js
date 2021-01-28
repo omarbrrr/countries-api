94f67ec52e018d1a5eda155c288ad1bd1f497b81
Omar Benavides <omar.benavides.rmz@gmail.com>
2021-01-28 03:11:00 -0600

    Change display format of Population number

--- src/components/Country.js
+++ src/components/Country.js
@@ -1,17 +1,24 @@
-import React from 'react'
+import React from "react";
 
 export default function Country({ country }) {
   const { flag, name, population, region, capital } = country;
 
   return (
     <div className="country">
-      <img src={flag} className='country__flag' alt=''/>
+      <img src={flag} className="country__flag" alt="" />
       <div className="country__content">
         <h2 className="country__title">{name}</h2>
-        <p className="country__subtitle">Population: <span className="country__value">{population}</span></p>
-        <p className="country__subtitle">Region: <span className="country__value">{region}</span></p>
-        <p className="country__subtitle">Capital: <span className="country__value">{capital}</span></p>
+        <p className="country__subtitle">
+          Population:{" "}
+          <span className="country__value">{population.toLocaleString()}</span>
+        </p>
+        <p className="country__subtitle">
+          Region: <span className="country__value">{region}</span>
+        </p>
+        <p className="country__subtitle">
+          Capital: <span className="country__value">{capital}</span>
+        </p>
       </div>
     </div>
-  )
+  );
 }
